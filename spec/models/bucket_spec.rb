e98056d98fe058d4d4fce4ebe8aeba53b763feff
Marcle Rodrigues <maarclee@gmail.com>
2019-08-30 10:46:34 -0300

    Fix bucket spec

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Bucket, type: :model do
     subject { Bucket.new(image: image_file) }
 
     it 'allows the user to delete' do
-      expect(subject.image.purge).not_to raise(FrozenError)
+      expect { subject.image.purge }.not_to raise_error(FrozenError)
     end
   end
 end
